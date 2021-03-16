02ca40970d253bc8329a0d1285d0c2d610f455c0
irunga1 <pabloirungaray@gmail.com>
2021-03-16 07:59:11 -0600

    cambio

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,8 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','session','upload','form_validation');
+// $autoload['libraries'] = array('database','session','upload','form_validation');
+$autoload['libraries'] = array('session','upload','form_validation');
 
 /*
 | -------------------------------------------------------------------
