441293933742ee5747fe92c9fb1482fd05cfd353
irunga1 <pabloirungaray@gmail.com>
2021-03-16 22:43:51 -0600

    cambio

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -2,52 +2,121 @@
 
 </section>
 <section class="tmMain">
-    <div class="row">
-        <div class="col-md-4">            
-        </div>
-        <div class="col-md-8">
-            <h1 class ="cdTitleImg"> </h1>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-9">
-		<div> <img src="<?php echo base_url()."public/img/gchl.png" ?>" alt=""></div>
-        <div class="row">			
-            <div class="col-md-4 cdNews">
-                <div class="image"></div>
-                <div class="text">
-                    <h3>lorem ipsum watsara watsare1</h3>
-                    <p>lorem ipsum watsara watsare bla bla lorem ipsum watsara watsare bla blalorem ipsum watsara watsare bla bla</p>
-                </div>
-            </div>
-            <div class="col-md-4 cdNews">
-            <div class="image"></div>
-                <div class="text">
-                    <h3>lorem ipsum watsara watsare2</h3>
-                    <p>lorem ipsum watsara watsare bla bla lorem ipsum watsara watsare bla blalorem ipsum watsara watsare bla bla</p>
-                </div>
-            </div>
-            <div class="col-md-4 cdNews">
-            <div class="image"></div>
-                <div class="text">
-                    <h3>lorem ipsum watsara watsare3</h3>
-                    <p>lorem ipsum watsara watsare bla bla lorem ipsum watsara watsare bla blalorem ipsum watsara watsare bla bla</p>
-                </div>
-            </div>
-        </div>
-        </div>
-    </div>
+	<div class="row">
+		<div class="col-md-4">
+		</div>
+		<div class="col-md-8">
+			<h1 class="cdTitleImg"> </h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-3"></div>
+		<div class="col-md-9">
+			<div> <img src="<?php echo base_url()."public/img/gchl.png" ?>" alt=""></div>
+			<div class="row">
+				<div class="col-md-4 cdNews">
+					<div class="image"></div>
+					<div class="text">
+						<h3>lorem ipsum watsara watsare1</h3>
+						<p>lorem ipsum watsara watsare bla bla lorem ipsum watsara watsare bla blalorem ipsum watsara
+							watsare bla bla</p>
+					</div>
+				</div>
+				<div class="col-md-4 cdNews">
+					<div class="image"></div>
+					<div class="text">
+						<h3>lorem ipsum watsara watsare2</h3>
+						<p>lorem ipsum watsara watsare bla bla lorem ipsum watsara watsare bla blalorem ipsum watsara
+							watsare bla bla</p>
+					</div>
+				</div>
+				<div class="col-md-4 cdNews">
+					<div class="image"></div>
+					<div class="text">
+						<h3>lorem ipsum watsara watsare3</h3>
+						<p>lorem ipsum watsara watsare bla bla lorem ipsum watsara watsare bla blalorem ipsum watsara
+							watsare bla bla</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
 <section class="tmMain">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>Guatemala</h1>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-4"></div>
-        <div class="col-md-4"></div>
-        <div class="col-md-4"></div>
-    </div>
+	<div class="row">
+		<div class="cdupside">
+		</div>
+		<div class="col-md-3"></div>
+		<div class="col-md-9">
+			<div class="cdTitle">
+				<h1>MENSAJE DEL PRESIDENTE</h1>
+			</div>
+			<div class="row">
+
+
+				<div class="col-md-8">
+					<p>Es un placer liderar la Cámara de Comercio de Guatemala en Estados Unidos (CADECOGUATE);
+						la cual funge como un promotor bilateral de relaciones comerciales de inversión y asociación
+						entre ambos países fundada en el año 2019. A través de nuestra extensa red de oficinas
+						en ambos países, proveemos soluciones de alto valor a las empresas afiliadas en los diversos
+						sectores estratégicos de acción, permitiéndoles generar un ambiente propicio para
+						la consolidación de negocios entre ambos países.
+					</p>
+					<p>
+						Por medio de Cámara se impulsarán importantes iniciativas en materia de: Comercio Exterior,
+						Seguridad en el Transporte, Eficiencia en la Cadena de Suministro y Cobertura de Divisas
+						entre otros. Estamos seguros que los ejes planteados permitirán procesos de gestión
+						más eficientes al interior de las empresas redundando en un mayor desarrollo y competitividad.
+						Impulsaremos a las PYMES que buscan desarrollarse o consolidarse en materia de
+						Comercio Exterior, ofreciéndoles una membresía especial y contarán con un Centro
+						de Información que facilitará su incursión en el mercado norteamericano.</p>
+					<p>En mi carácter de Presidente de Cámara de Comercio de Guatemala
+						en Estados Unidos / Guatemala Chamber of Commerce (Cadecoguate); reitero mi compromiso
+						y apoyo con la organización y sus afiliados y me permito darles la más cordial bienvenida.
+					</p>
+					<p>
+						<h6><strong>Lic. Rafael Oscal</strong></h6>
+						<h6>Presidente/CEO</h6>
+						<h6>Cámara de Comercio Guatemala USA</h6>
+					</p>
+				</div>
+
+				<div class="col-md-4">
+				</div>
+			</div>
+		</div>
+
+
+	</div>
+</section>
+<section class="tmMain">
+	<div class="cdupside">
+		<div class="row">
+			<div class="col-md-3"></div>
+			<div class="col-md-9">
+				<div id="cdBlueBackground" class="col-md-8">
+					<h4>Historia</h4>
+					<p>La Cámara de Comercio Guatemala USA (CADECOGUATE) fue establecida en Los Ángeles CA
+						D.C. en 2019 un grupo de distinguidos empresarios guatemaltecos y estadounidenses, como
+						una asociación de negocios sin fines de lucro, con el propósito de promover las relaciones
+						comerciales, de inversión e iniciativas de asociación entre empresas de ambos países. La
+						Cámara se ha consolidado como una organización líder en los negocios y una fundación cultural
+						y educativa entre Guatemala y Estados Unidos en la Ciudad de Los Ángeles.
+					</p>
+					<p>
+						En la actualidad, Cámara de Comercio Guatemalteca en Estados Unidos opera a través de una
+						red en las principales ciudades de Guatemala y los Estados Unidos como: Los Ángeles oficina
+						Matriz y muy pronto Dallas, Nueva York, Chicago, Seattle, Miami, Houston.</p>
+					<p>
+						La cámara se ha involucrado en actividades importantes para movilizar el apoyo nacional e in
+						ternacional para la aprobación en el Congreso de los Estados Unidos de este histórico Tratado
+						Comercial entre los países de: Estados Unidos, Guatemala y Canadá.
+					</p>
+
+				</div>
+				<div class="col-md-4"></div>
+			</div>
+
+		</div>
+	</div>
 </section>
