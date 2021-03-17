75d4cc2964769ab997923cc8a929175fd02c4c4d
irunga1 <pabloirungaray@gmail.com>
2021-03-16 19:12:25 -0600

    cambio

--- public/cd_template_header.php
+++ public/cd_template_header.php
@@ -6,10 +6,10 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="<?php echo base_url("/public/js/main.js"); ?>"></script>
-    <script src="<?php echo base_url("/public/js/menu.js"); ?>"></script>
+    <script src="<?php echo base_url("public/js/main.js"); ?>"></script>
+    <script src="<?php echo base_url("public/js/menu.js"); ?>"></script>
     
-    <link rel="stylesheet" href="http://localhost/wpirunga1/public/css/main.css">
+    <link rel="stylesheet" href="<?php echo base_url("public/css/main.css"); ?>">
 </head>
 <body>
 <span id="fixed-div" style="font-size:30px;cursor:pointer" onclick="obj1.openNav();">&#9776;</span>
