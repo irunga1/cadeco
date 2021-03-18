761dda9809107e151c114eb70b86c65be2b4ca04
irunga1 <pabloirungaray@gmail.com>
2021-03-18 09:41:18 -0600

    cambio

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/wpirunga1';
-$config['base_url'] = 'http://localhost/cadeco';
-// $config['base_url'] = 'http://52.207.9.238/cadeco';
+// $config['base_url'] = 'http://localhost/wpirunga1';
+// $config['base_url'] = 'http://localhost/cadeco';
+$config['base_url'] = 'http://52.207.9.238/cadeco';
 
 /*
 |--------------------------------------------------------------------------
