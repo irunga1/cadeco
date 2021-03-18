ede055d0af14d8e3c60dd99985e55cd33faba1e4
irunga1 <pabloirungaray@gmail.com>
2021-03-17 22:31:26 -0600

    cambio ui

---
+++ application/views/directorio/index.php
@@ -0,0 +1,66 @@
+<section class="tmMain">
+	<div class="cdupside">
+	</div>
+
+	<div class="row">
+		<div class="col-md-3"></div>
+		<div class="col-md-9">
+			<h2> <strong>DIRECTORIO</strong> </h2>
+			<hr>
+			<div class="dirContainer">
+				<div class="cdPersonItm">
+					<div class="cdItmImg">
+						<img src="<?php echo base_url("public/img/1.png") ?>" alt="">
+					</div>
+					<div class="info">
+					<strong>RAFAEL OSCAL</strong><br>
+					roscal@cadecoguate.com <br>
+					https://www.linkedin.com/in/rafaelo-scal/ <br>
+
+					<div class="cdOtherInfo">
+					CROSS PROMOTION MARKETING,
+					INC.
+					LOS ANGELES CA
+					</div>
+
+				
+					</div>
+				</div>
+				<div class="cdPersonItm">
+					<div class="cdItmImg">
+						<img src="<?php echo base_url("public/img/1.png") ?>" alt="">
+					</div>
+					<div class="info"></div>
+				</div>
+				<div class="cdPersonItm">
+					<div class="cdItmImg">
+						<img src="<?php echo base_url("public/img/1.png") ?>" alt="">
+					</div>
+					<div class="info"></div>
+				</div>
+				<div class="cdPersonItm">
+					<div class="cdItmImg">
+						<img src="<?php echo base_url("public/img/1.png") ?>" alt="">
+					</div>
+					<div class="info"></div>
+				</div>
+				<div class="cdPersonItm">
+					<div class="cdItmImg">
+						<img src="<?php echo base_url("public/img/1.png") ?>" alt="">
+					</div>
+					<div class="info"></div>
+				</div>
+				<div class="cdPersonItm">
+					<div class="cdItmImg">
+						<img src="<?php echo base_url("public/img/1.png") ?>" alt="">
+					</div>
+					<div class="info"></div>
+				</div>
+
+				
+				
+			</div>
+
+		</div>
+	</div>
+</section>
