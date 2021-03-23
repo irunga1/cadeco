fbc5f7c0c6567bfd4e41e8d1bd9b057e15db44dd
irunga1 <pabloirungaray@gmail.com>
2021-03-22 22:26:25 -0600

    cambio miembros y membresias

--- application/views/miembros/index.php
+++ application/views/miembros/index.php
@@ -1,32 +1,67 @@
 <section class="tmMain">
 	<div class="cdupside"></div>
-	<div class="row fullsize" >
-		<div class="col-md-3">
-		</div>
-		<div class="col-md-9">
+	<div class="row" >
+		<div class="col-md-12 fullsize">
 			<h2> <strong>Organizaciones Afiliadas</strong> </h2>
 			<hr>
-			<div class="row fullsize">
+			<div class="row ">
 				<div class="col-md-12">
 					<div class="imageContainer">
-						<img src="<?php echo base_url("public/img/miembros/rs=h_100.jpeg")?>" alt="" class="cdImgAf">
-						<img src="<?php echo base_url("public/img/miembros/rs=h_100.jpeg")?>" alt="" class="cdImgAf">
-						<img src="<?php echo base_url("public/img/miembros/rs=h_100.jpeg")?>" alt="" class="cdImgAf">
-						<img src="<?php echo base_url("public/img/miembros/rs=h_100.jpeg")?>" alt="" class="cdImgAf">
-						<br>
-						<img src="<?php echo base_url("public/img/miembros/rs=h_100.jpeg")?>" alt="" class="cdImgAf">
-						<img src="<?php echo base_url("public/img/miembros/rs=h_100.jpeg")?>" alt="" class="cdImgAf">
-						<img src="<?php echo base_url("public/img/miembros/rs=h_100.jpeg")?>" alt="" class="cdImgAf">
-						<img src="<?php echo base_url("public/img/miembros/rs=h_100.jpeg")?>" alt="" class="cdImgAf">
-						<br>
-						<img src="<?php echo base_url("public/img/miembros/rs=h_100.jpeg")?>" alt="" class="cdImgAf">
-						<img src="<?php echo base_url("public/img/miembros/rs=h_100.jpeg")?>" alt="" class="cdImgAf">
-						<img src="<?php echo base_url("public/img/miembros/rs=h_100.jpeg")?>" alt="" class="cdImgAf">
-						<img src="<?php echo base_url("public/img/miembros/rs=h_100.jpeg")?>" alt="" class="cdImgAf">
+						<?php
+							for($i=1;$i<14;$i++){
+								 echo "<img src='".base_url("public/img/marcas/1-$i.jpeg")."' alt='' class='cdImgAf'>";
+								 $a = $i%5;
+								 if($a==0){
+									echo "<hr>";
+								 }
+							}						
+						?>
 					</div>
-
-				</div>
-				
+				</div>				
+			</div>
+		</div>
+	</div>
+	<div class="cdupside"></div>
+	<div class="row" >
+		<div class="col-md-12 fullsize">
+			<h2> <strong>Organizaciones Afiliadas</strong> </h2>
+			<hr>
+			<div class="row ">
+				<div class="col-md-12">
+					<div class="imageContainer">
+						<?php
+							for($i=1;$i<4;$i++){
+								 echo "<img src='".base_url("public/img/marcas/3-$i.jpg")."' alt='' class='cdImgAf'>";
+								 $a = $i%5;
+								 if($a==0){
+									echo "<hr>";
+								 }
+							}						
+						?>
+					</div>
+				</div>				
+			</div>
+		</div>
+	</div>
+	<div class="cdupside"></div>
+	<div class="row" >
+		<div class="col-md-12 fullsize">
+			<h2> <strong>Organizaciones Afiliadas</strong> </h2>
+			<hr>
+			<div class="row ">
+				<div class="col-md-12">
+					<div class="imageContainer">
+						<?php
+							for($i=1;$i<4;$i++){
+								 echo "<img src='".base_url("public/img/marcas/2-$i.png")."' alt='' class='cdImgAf'>";
+								 $a = $i%5;
+								 if($a==0){
+									echo "<hr>";
+								 }
+							}						
+						?>
+					</div>
+				</div>				
 			</div>
 		</div>
 	</div>
