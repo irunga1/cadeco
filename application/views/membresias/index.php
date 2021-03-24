1eec02fc79af251a95e02e938929a4c94ca4fea7
irunga1 <pabloirungaray@gmail.com>
2021-03-23 21:39:19 -0600

    cambios totales

--- application/views/membresias/index.php
+++ application/views/membresias/index.php
@@ -1,12 +1,12 @@
 <section class="tmMain">
 	<div class="row">
-		<div class="col-md-3">
-		</div>
-		<div class="col-md-9">
+
+		<div class="col-md-12 fullsize">
 			<div class="cdupside">
 			</div>
 			<h2><strong>MEMBRESÍAS</strong></h2>
 			<hr>
+			<p>Inscríbete y forma parte como socio activo de CADECOGUATE</p>
 			<button class ="btn btn-info"></button>
 			<div class="row">
 				<div class="dirContainer">
@@ -104,35 +104,6 @@
 					
 				</div>
 
-			</div>
-
-		</div>
-	</div>
-</section>
-<section class="tmMain">
-	<div class="row">
-		<div class="col-md-3">
-		</div>
-		<div class="col-md-9">
-			<div class="cdupside">
-			</div>
-			<h2><strong>CONTACTANOS</strong></h2>
-			<hr>
-			<div class="row">
-				<div class="col-md-6">
-					<h4>
-					¡Descubre más formas de ayudar!
-					</h4>
-					<p>
-					Hay muchas maneras de apoyar nuestra Camara.
-					Contáctenos para obtener más información sobre
-					oportunidades de voluntariado, eventos de recaudación
-					de fondos y formas de transmitir nuestro mensaje a
-					sus amigos y familiares.
-					</p>
-				</div>
-				<div class="col-md-6">
-				</div>
 			</div>
 
 		</div>
